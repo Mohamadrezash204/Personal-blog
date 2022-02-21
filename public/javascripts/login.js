70b91f6e69138fdb9654773c5d7c0cb74e859b7a
Mohamadrezash204 <mohamadreza.sh138110@gmail.com>
2022-02-21 17:01:00 +0330

    reform  validation front

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -10,7 +10,11 @@ $(document).ready(function() {
     })
     $("#PasswordInput").focusout(function() {
         var PasswordInput = $("#PasswordInput").val();
+        const regex = new RegExp("^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$")
+        if (regex.test(PasswordInput)) {
+            $("#Passwordvalid").text("is validation")
 
-
+        } else
+            $("#Passwordvalid").text("not valid")
     })
 });

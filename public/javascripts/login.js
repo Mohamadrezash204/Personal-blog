247e40bb9fbc9b5318111eea4e3dab146943c3f5
Mohamadrezash204 <mohamadreza.sh138110@gmail.com>
2022-02-27 17:41:44 +0330

    add validation

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     $("#UsernameInput").focusout(function() {
         var UsernameInput = $("#UsernameInput").val();
-        const regex = new RegExp("^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$")
+        const regex = new RegExp(/^[A-Za-z0-9_\.]+$/)
         if (regex.test(UsernameInput)) {
             $("#Usernamevalid").text("is validation")
 
@@ -10,7 +10,7 @@ $(document).ready(function() {
     })
     $("#PasswordInput").focusout(function() {
         var PasswordInput = $("#PasswordInput").val();
-        const regex = new RegExp("^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$")
+        const regex = new RegExp(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9_])/)
         if (regex.test(PasswordInput)) {
             $("#Passwordvalid").text("is validation")
 
--- public/javascripts/test.js
+++ public/javascripts/test.js
@@ -1,1 +1,19 @@
-console.log();
+var bcrypt = require('bcrypt');
+
+bcrypt.hash('mypassword', 10, function(err, hash) {
+    if (err) { throw (err); }
+    console.log(hash);
+    bcrypt.compare('mypassword', hash, function(err, result) {
+        if (err) { throw (err); }
+        console.log(result);
+    });
+});
+// const hash = "$2b$10$J4lBMgT1UcrfHGqLeLDWqO0knhWqK/Z4too39OpQopsyMD/f4g8m6"
+
+
+// // const hashchek = bcrypt.compare('mypassword', hash, function(err, result) {
+// //     if (err) { throw (err); }
+// //     return result
+// // });
+// // console.log(hashchek);
+// if (bcrypt.compare('mypasswor', hash)) console.log(false);

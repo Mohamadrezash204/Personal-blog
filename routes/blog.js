67ddd78a3f18be05163034d97338b88e64e6e32d
Mohamadrezash204 <mohamadreza.sh138110@gmail.com>
2022-02-27 17:44:10 +0330

    show article with author

--- routes/blog.js
+++ routes/blog.js
@@ -1,50 +1,17 @@
 const blog = require('express').Router();
 const Article = require('../models/Articles')
 const { isLoggedIn } = require('../middlewares/auth')
-
-// const articles = article.find({}, (err, articles) => {
-//     if (err) return logger.error(err)
-//     return articles;
-// })
-
+const { editArticle, create, myArticles } = require('../controller/blog')
 
 blog.get('/', (req, res) => {
-    Article.find({}).populate('author').exec((err, articles) => {
+    Article.find({}).sort({ createdAt: 'descending' }).populate('author').lean().exec((err, articles) => {
         if (err) return logger.error(err)
-        res.render('blog.ejs', { articles });
-        console.log(articles.length)
-    })
-
-})
-
-blog.post('/create', function(req, res) {
-    console.log(req.body);
-    if (!!req.session.user) {
-        if (!!req.body.title && !!req.body.content) {
-            Article.create({
-                    title: req.body.title,
-                    content: req.body.content,
-                    author: req.session.user
-                },
-                function(err, result) {
-                    if (err) return res.send(err);
-                    if (result)
-                        res.redirect("/home");
-                    //res.send(result);
-                })
-        }
-    } else {
-        res.send((" شما وارد نشده اید برای نوشتن مقاله باید وارد شوید"))
-    }
-})
-
-blog.get('/myArticles', isLoggedIn, function(req, res) {
-    const user = req.session.user
-    Article.findOne({ author: user }, function(err, articles) {
-        if (err) return res.send(err)
-        if (articles.length === 0) return res.send({ masage: "you dont have any articles" })
-        res.render("myarticles", { articles })
+        res.render('blog.ejs', { articles: articles });
     })
 })
+
+blog.post('/create', isLoggedIn, create)
+blog.get('/myArticles', isLoggedIn, myArticles)
+blog.post("/editArticle", isLoggedIn, editArticle)
 
 module.exports = blog;

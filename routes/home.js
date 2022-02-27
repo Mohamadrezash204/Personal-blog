682800bd0140333035c6635d91d404a6f7a2a112
Mohamadrezash204 <mohamadreza.sh138110@gmail.com>
2022-02-27 17:46:46 +0330

    cleaning

--- routes/home.js
+++ routes/home.js
@@ -1,23 +1,20 @@
 const home = require('express').Router();
-
-
-/* GET home page. */
-// home.get('/home', function(req, res) {
-//     res.render('home', { title: 'home', lastName, firstName, avatar, rol, status });
-
-// })
+const Articles = require('../models/Articles');
 
 home.get("/", function(req, res) {
     if (req.session.user) {
+        Articles.findOne({ top: true }, function(err, article) {
+            if (err) return res.send(err);
 
-        if (req.session.user.role == "user") {
-            res.render('home', { title: 'home', lastName: req.session.user.lastName, firstName: req.session.user.firstName, avatar: req.session.user.avatar, role: "کاربر", status: "پروفایل ", statuses: "/dashboard" });
-        }
-        if (req.session.user.role === "admin") {
-            res.render('home', { title: 'home', lastName: req.session.user.lastName, firstName: req.session.user.firstName, avatar: req.session.user.avatar, role: "ادمین", status: "کاربرها", statuses: "/admin" });
-        }
+            if (req.session.user.role == "user") {
+                res.render('home', { articles: article, title: 'home', lastName: req.session.user.lastName, firstName: req.session.user.firstName, avatar: req.session.user.avatar, role: "کاربر", status: "پروفایل", statuses: "/dashboard" });
+            }
+            if (req.session.user.role === "admin") {
+                res.render('home', { articles: article, title: 'home', lastName: req.session.user.lastName, firstName: req.session.user.firstName, avatar: req.session.user.avatar, role: "ادمین", status: "کاربرها", statuses: "/admin" });
+            }
+        })
     } else
-        res.render('home', { title: 'home', lastName: "نشده‌اید", firstName: "وارد", avatar: './images/avatar/avatar.jpg', role: "کاربر", status: " ورود", statuses: "/auth/login" });
+        res.render('home', { articles: {}, title: 'home', lastName: "نشده‌اید", firstName: "وارد", avatar: './images/avatar/avatar.jpg', role: "کاربر", status: "ورود", statuses: "/auth/login" });
 
 })
 
